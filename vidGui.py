bd7bb40e011c071d8fef7698a2807fdd109d17d6
jlarsch <jlarsch@neuro.mpg.de>
2019-05-28 11:53:58 +0200

    Updated Demo Video Gui for Miguel competition

--- vidGui.py
+++ vidGui.py
@@ -3,17 +3,57 @@
 Created on Fri Feb 10 10:55:23 2017
 
 @author: jlarsch
+
+
+This script creates demo videos for loom experiments. Works with Miguel's most recent competition
+experiments as of May 2019.
+
+Requires the raw video file, it's posText output file and the ROIdef file all in the same folder.
+
+Run this script in Spyder (for Python 3)
+
+In the file dialog, select the video file.
+
+Now use the slider 'currFrame' so slide to the frame where you want to demo video to start.
+
+Use left mouse click into the video to begin video playback, right mouse click to stop.
+
+WHILE PLAYBACK IS RUNNING, hit keyboard key 'v' (for video). This will begin saving the current playback.
+(You won't notice any change, except maybe playback slows down).
+
+When the video has reached the desired frame, hit keyboard key 'v' again to end the recording.
+
+You can now stop the playback using the mouse right click.
+
+You should now have a new file in the video folder with the starting frame in the file name.
+
+You can now slide to a new frame and record another video without re-running the script.
+
+When the order of clicking playback start and recording the video is messed up,
+python can get stuck with an empty video file that it struggles to release.
+In this case, it is easiest to restart the kernel and re-run the script.
+
+You can also define the starting frame precisely when running the script via  self.currFrame='frame number'.
+
+Once the video gui is loaded, you can toggle rereadTxt=0 and rere-run the script without closing the gui.
+This way, you can set currFrame directly to a desired value.
+
+length = 50000 (in class vidGui(object): #This limits how far you can scroll into the video.
+In the past, the gui struggled to seek deeply into videos but 50.000 or even 100.000 frames should be OK.
+
+
 """
 
+
+
 import cv2
-import tkFileDialog
+from tkinter import filedialog
 import os
-import matplotlib.pyplot as plt
-import models.experiment as xp
 import glob
 import numpy as np
 import pandas as pd
-#import datetime
+
+rereadTxt=1 #set to 1 for initial start, can toggle to 0 once data is loaded.
 
 class settings(object):
 
@@ -21,7 +61,7 @@ class settings(object):
                  endFrame=10):
         self.startFrame=startFrame
         self.endFrame=endFrame      
-        self.currFrame=5000
+        self.currFrame=12000 # Can define the starting frame for the demo recording here.
         self.run=False
         self.vidRec=False
         self.haveVid=False
@@ -32,39 +72,23 @@ class vidGui(object):
     def __init__(self, path,anMat,sMat=[],df_roi=[]):
 
         self.settings=settings()
-        #self.df=df
-        #self.skipNan=e1.skipNanInd
         self.path = path
         self.cap = cv2.VideoCapture(path)
         self.im=[]
         self.df_roi=df_roi
         self.anMat=anMat
         self.sMat=sMat
-        #self.t1=e1.rawTra[:,0,:].copy()
-        #self.t2=e2.rawTra[:,0,:].copy()
-        #self.t1b=e1.rawTra[:,1,:].copy()
-        #self.t2b=e2.rawTra[:,1,:].copy()
-        #self.t1[:,0]=self.t1[:,0]+512        
-        #self.t1b[:,0]=self.t1b[:,0]+512  
         cv2.namedWindow(self.window_name,cv2.WINDOW_AUTOSIZE)
         self.desiredWidth=1024
         self.desiredheight=1024
         
-        #cv2.createTrackbar("Min", "Threshold", &threshMin, 255, on_trackbar);
 
-        #cv2.namedWindow(self.window_name,cv2.WINDOW_AUTOSIZE)
-        length = 30000#int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        length = 50000 #This limits how far you can scroll into the video. see notes above.
         cv2.setMouseCallback(self.window_name, self.startStop)
-        cv2.createTrackbar("startFrame", self.window_name,
-                           self.settings.startFrame, 1000,
-                           self.set_startFrame)
-        cv2.createTrackbar("endFrame", self.window_name,
-                           self.settings.endFrame, 1000,
-                           self.set_endFrame)
+
         cv2.createTrackbar("currFrame", self.window_name,
                            self.settings.currFrame, length,
                            self.set_currFrame)
-        #cv2.resizeWindow(self.window_name, self.desiredWidth,self.desiredheight)               
     def startStop(self,event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.settings.run=True
@@ -75,7 +99,7 @@ class vidGui(object):
             
                 key = cv2.waitKey(1) & 0xFF
                  
-                if key == ord("v"):
+                if key == ord("v"): #for video recording: toggle start-stop
                   
                     self.settings.vidRec = not self.settings.vidRec
                     
@@ -83,7 +107,6 @@ class vidGui(object):
                         if not self.settings.haveVid:
                                              
                             p, tail = os.path.split(self.path)
-                            #fn=p+"\\episodeVid_frame_%07d_%s.avi"%(f,self.currEpi)
                             fn=p+"\\episodeVid_frame_%07d.avi"%(f)
                             fn=os.path.normpath(fn)
                             fourcc=cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
@@ -95,20 +118,13 @@ class vidGui(object):
                             self.vOut.release()
                             self.settings.haveVid=False
      
-                if key == ord("s"):
+                if key == ord("s"): #start-stop video: redundant with mouse click.
                     self.settings.run=False
                 cv2.setTrackbarPos("currFrame", self.window_name,f)
                 
         elif event == cv2.EVENT_RBUTTONDOWN:
             self.settings.run=False
                       
-    def set_startFrame(self, f1):
-        self.settings.startFrame = f1
-        self.updateFrame()
-
-    def set_endFrame(self, f2):
-        self.settings.endFrame = f2
-        self.updateFrame()
 
     def set_currFrame(self, f3):
         
@@ -116,19 +132,21 @@ class vidGui(object):
         self.updateFrame()
     def updateFrame(self):
         
-         
-        tail=1
-        tailStep=1.0
-        #dotScale=1
-        pathDotSize=0#4 
         stimDotSize=4
         stimDotSize2=4
-        animalDotSize=0#6 #not showing animal path for now
+
+        # some of the commented stuff below can be used to also draw the path of the animal.         
+        #tail=1 #length of animal path
+        #tailStep=1.0# interval between path data points to plot
+        #dotScale=1
+        #pathDotSize=0#4 
+        #animalDotSize=0#6 #not showing animal path for now
+        
         self.cap.set(cv2.CAP_PROP_POS_FRAMES,self.settings.currFrame)
         self.im=255-self.cap.read()[1]
-        fs= self.settings.currFrame-np.mod(self.settings.currFrame,tailStep)
+        #fs= self.settings.currFrame -np.mod(self.settings.currFrame,tailStep)
         #self.currEpi=self.df['episode'].ix[np.where(self.df['epStart']>fs)[0][0]-1][1:]
-        r=np.arange(fs-tail,fs,tailStep).astype('int')
+        #r=np.arange(fs-tail,fs,tailStep).astype('int')
         s=sMat[0,self.settings.currFrame,0]
         
         if ~np.isnan(s):
@@ -136,12 +154,13 @@ class vidGui(object):
             stimDotSize2=int(sMat[0,self.settings.currFrame,1])
         #DRAW path history
         #for f in r:
-        for f in [self.settings.currFrame]:
-            opacity=((fs-f)/float(tail))
+        #for f in [self.settings.currFrame]:
+        f=self.settings.currFrame
+            #opacity=((fs-f)/float(tail))
             
-            for an in range(9):
-                center=tuple(self.anMat[an,f,[0,1]].astype('int'))
-                cv2.circle(self.im, center,pathDotSize , (opacity*255,opacity*255,255), -1) 
+            #for an in range(9):
+            #    center=tuple(self.anMat[an,f,[0,1]].astype('int'))
+                #cv2.circle(self.im, center,pathDotSize , (opacity*255,opacity*255,255), -1) 
                 #center=tuple(self.anMat[an,f,[2,3]].astype('int'))
                 #cv2.circle(self.im, center, stimDotSize, (opacity*255,opacity*255,opacity*255), -1) 
                 #center=tuple(self.anMat[an,f,[4,5]].astype('int'))
@@ -151,57 +170,50 @@ class vidGui(object):
         #DRAW Current frame animal positions 
         for an in range(9):
             opacity=0
-            center=tuple(self.anMat[an,f,[0,1]].astype('int'))
-            cv2.circle(self.im, center, animalDotSize, (0,1,0), -1)
+            #center=tuple(self.anMat[an,f,[0,1]].astype('int'))
+            #cv2.circle(self.im, center, animalDotSize, (0,1,0), -1)
             center=tuple(self.anMat[an,f,[2,3]].astype('int'))
             cv2.circle(self.im, center, stimDotSize, (opacity*255,opacity*255,opacity*255), -1) 
             center=tuple(self.anMat[an,f,[4,5]].astype('int'))
             cv2.circle(self.im, center, stimDotSize2, (opacity*255,opacity*255,opacity*255), -1)  
-           
 
         #if 'skype' in self.currEpi:
         #    cv2.circle(self.im, center, 6, (255,opacity*255,opacity*255), 1)
-        
-
         
         #DRAW DISH BOUNDARIES
         for index, row in self.df_roi.iterrows():
             center=tuple((row.x_center,row.y_center))
             rad=row.radius
             cv2.circle(self.im, center, rad, (0,0,0), 2)
-        #center=tuple((256+512,256))
-        #cv2.circle(self.im, center, 240, (0,0,0), 2)
-        #center=tuple((256+2*512,256))
-        #cv2.circle(self.im, center, 240, (0,0,0), 2)
         
+        #Add frame and size info Text to video frame.
         font = cv2.FONT_HERSHEY_SIMPLEX
         #cv2.putText(self.im,self.currEpi,(450,20), font, 0.6,(0,0,0),2)  
         
         frInfo='Frame #: '+str(self.settings.currFrame)
-        cv2.putText(self.im,frInfo,(450,40), font, 0.4,(0,0,0),2)
+        cv2.putText(self.im,frInfo,(500,40), font, 0.4,(0,0,0),2)
         
         miliseconds=self.settings.currFrame*(100/3.0)
         seconds,ms=divmod(miliseconds, 1000)
         m, s = divmod(seconds, 60)
         h, m = divmod(m, 60)
         timeInfo= "%02d:%02d:%02d:%03d" % (h, m, s,ms)
-        cv2.putText(self.im,timeInfo,(450,60), font, 0.4,(0,0,0),2)
+        cv2.putText(self.im,timeInfo,(500,60), font, 0.4,(0,0,0),2)
         timeInfo2='(hh:mm:ss:ms)'
-        cv2.putText(self.im,timeInfo2,(450,80), font, 0.4,(0,0,0),2)
+        cv2.putText(self.im,timeInfo2,(500,80), font, 0.4,(0,0,0),2)
 
         #cv2.line(self.im, tuple((1024,0)), tuple((1024,512)), (0,0,0))        
-        
         #cv2.putText(self.im,"Arena 1",(220,60), font, 0.6,(0,0,0),2)
         #cv2.putText(self.im,"Arena 2",(220+512,60), font, 0.6,(0,0,0),2)
-        cv2.putText(self.im,str(stimDotSize),(200+1024,60), font, 0.6,(0,0,0),2)
-        cv2.putText(self.im,str(stimDotSize2),(200+1024,80), font, 0.6,(0,0,0),2)
+        cv2.putText(self.im,'DotSize1: ~'+str(stimDotSize/2)+' mm',(650,60), font, 0.6,(0,0,0),2)
+        cv2.putText(self.im,'DotSize2: ~'+str(stimDotSize2/2)+' mm',(650,80), font, 0.6,(0,0,0),2)
         #cv2.resizeWindow(self.window_name, self.desiredWidth,self.desiredheight)
         #newWidth=1024
         #r = newWidth / self.im.shape[1]
         #dim = (newWidth, int(self.im.shape[0] * r))
  
         # perform the actual resizing of the image and show it
-        dim=(1024,1024)
+        dim=(1024,1024) #for screen scaling. this does not affect the saved video
         resized = cv2.resize(self.im, dim)
          
         cv2.imshow(self.window_name, resized)
@@ -209,23 +221,14 @@ class vidGui(object):
         if self.settings.vidRec:
            
             wr=self.im.astype('uint8')
-
             self.vOut.write(wr)
 
-        
-#p='D:\\data\\b\\2017\\20170131_VR_skypeVsTrefoil\\01_skypeVsTrefoil_blackDisk002\\'
-
-   #D:\\data\\b\\2017\\20170420_miguel_competitionTest\\1\\PositionTxt_allROI2017-04-20T09_22_51.txt 
-
-rereadTxt=0
-
-
 if rereadTxt:
-    lines=67
-    empty=np.repeat('NaN',lines)
-    empty=' '.join(empty)
-    p='D:\\data\\b\\2017\\20170503_miguel_competition\\1\\'
-    avi_path = tkFileDialog.askopenfilename(initialdir=os.path.normpath(p))    
+    lines=67                        #for compatibility with older files
+    empty=np.repeat('NaN',lines)    #for compatibility with older files
+    empty=' '.join(empty)           #for compatibility with older files
+    p='R:\\johannes\\from Miguel\\video to annotate\\' #starting directory for file dialog
+    avi_path = filedialog.askopenfilename(initialdir=os.path.normpath(p))    
     p, tail = os.path.split(avi_path)
     txt_path=glob.glob(p+'\\Position*.txt')[0]
     roi_path=glob.glob(p+'\\ROI*.csv')[0]
@@ -243,7 +246,7 @@ if rereadTxt:
         mat=np.loadtxt((x if len(x)>6 else empty for x in f ),delimiter=' ')
     #epiLen=int(np.median(np.diff(np.where(mat[:-1,-1]!=mat[1:,-1]))))
     epiLen=int(np.median(np.diff(np.where((mat[:-1,-1]!=mat[1:,-1]) * ((mat[:-1,-1])==0)))))
-    print epiLen
+    print(epiLen)
     #ind=np.arange(149+epiLen,mat.shape[0],epiLen)
     #fixCol=[3,4,5,6,10,11,12,13,17,18,19,20,24,25,26,27,28,29]
     #for fc in fixCol:
@@ -252,11 +255,14 @@ if rereadTxt:
     anMat=[]
     sMat=[]
     for an in range(9):
-        tmp=np.array(mat[:,an*7+np.array([0,1,3,4,5,6])])
+        #pull columns corresponding to position of animal and its two stimuli
+        tmp=np.array(mat[:,an*7+np.array([0,1,3,4,5,6])]) 
+        #Add arena offset
         tmp[:,[0,2,4]]=tmp[:,[0,2,4]]+df_roi.x_topLeft[an]
         tmp[:,[1,3,5]]=tmp[:,[1,3,5]]+df_roi.y_topLeft[an]
         anMat.append(tmp)
-        tmp=np.array(mat[:,[-4,-3]])
+        #pull size for both dots - this is the same for animals.
+        tmp=np.array(mat[:,[-6,-5]]) #Size columns for the two stimuli
         sMat.append(tmp)
         #df['episode']=np.repeat(np.arange(mat.shape[0]/float(epiLen)),epiLen)
         #dfAll.append(df.copy())
